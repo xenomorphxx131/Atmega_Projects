005b236725622eecdb86b21a917756607c8dfa36
Steve M <Xenomorphxx131@gmail.com>
2023-09-09 17:00:26 -0400

    Missing f in f string

--- Awesomefaucet/Python/flash.py
+++ Awesomefaucet/Python/flash.py
@@ -1,4 +1,4 @@
-import serial, time, sys, os, subprocess
+import serial, time, sys, os, subprocess, pathlib
 
 os.system('cls')        #WINDOWS
 #os.system('clear')     #LINUX
@@ -11,7 +11,10 @@ def print_banner(title):
     print(u"\u2514" + u"\u2500" * 70 + u"\u2518")
     
 MCU = 'atmega32u4'
+
 TARGET = '../src/Awesomefaucet'
+TARGET_FILE =  pathlib.Path(__file__).parent.resolve().__str__() + "\\" + TARGET
+
 print_banner("Flashing Board with Latest Firmware....")
 comport_number = input("Enter COM Port Number, hit [ENTER] for fresh unit: COM")
 if comport_number != "":
@@ -25,20 +28,20 @@ else:
     virginboard = True
 
 print_banner("Begin Reprogramming Procedure...")
-COMMAND1 = "batchisp -hardware usb -device {} -operation erase f".format(MCU)
-COMMAND2 = "batchisp -hardware usb -device {} -operation loadbuffer {}.hex program".format(MCU, TARGET)
-COMMAND3 = "batchisp -hardware usb -device {} -operation start reset 0".format(MCU)
+COMMAND1 = f"batchisp -hardware usb -device {MCU} -operation erase f"
+COMMAND2 = f"batchisp -hardware usb -device {MCU} -operation loadbuffer {TARGET_FILE}.hex program"
+COMMAND3 = f"batchisp -hardware usb -device {MCU} -operation start reset 0"
 
 erase = subprocess.Popen(COMMAND1)
-while erase.poll() is None:
+while erase.poll() == None:
     time.sleep(0.01)
 
 flash = subprocess.Popen(COMMAND2)
-while flash.poll() is None:
+while flash.poll() == None:
     time.sleep(0.01)
  
 reset = subprocess.Popen(COMMAND3)
-while reset.poll() is None:
+while reset.poll() == None:
     time.sleep(0.01)
     
 print_banner("Restarting Device....")
@@ -48,10 +51,11 @@ os.system('cls')        #Windows only
 
 if virginboard:
     comport_number = input("Enter COM Port Number that Appeared: COM")
-port = serial.Serial("COM{}".format(comport_number), timeout = 4)
-port.write(b"SYSTem:VERSion?")
+port = serial.Serial(f"COM{comport_number}", timeout=4)
+port.write(b"*IDN?")
+# port.write(b"SYSTem:VERSion?")
 response = port.read(100)
 port.close()
 print_banner("Firmware Upgrade Complete!")
-print_banner("Response was: {}".format(response))
+print_banner(f"Response was: {response}")
 input("\nHit Enter to Quit")
