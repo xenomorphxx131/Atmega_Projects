19925c2d8fcbb753e3821ef9a40dc4edba982e70
Steve M <Xenomorphxx131@gmail.com>
2024-01-02 11:02:03 -0500

    Added comment to get rid of water as a dependency as a goal.

--- Awesomefaucet/src/Awesomefaucet_timers.h
+++ Awesomefaucet/src/Awesomefaucet_timers.h
@@ -9,7 +9,7 @@
 
 	#include <stdbool.h>                // Needed for booleans
 	#include <stdint.h>                 // Needed for integers
-	#include "Awesomefaucet_water.h"   	// Needed to control water
+	#include "Awesomefaucet_water.h"   	// Needed to control water - shouldn't be here!!!!!
 
 	void process_soft_timers(void);
 
