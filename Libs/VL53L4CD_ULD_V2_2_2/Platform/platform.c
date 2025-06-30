09752fa304dcf394e9f14523225defa39db16aad
Steve M <Xenomorphxx131@gmail.com>
2025-06-30 12:13:24 -0400

    Adding VL53L4CD since the 6180 sensor is being obsoleted.

---
+++ Libs/VL53L4CD_ULD_V2_2_2/Platform/platform.c
@@ -0,0 +1,82 @@
+/**
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+
+#include "platform.h"
+
+
+uint8_t VL53L4CD_RdDWord(Dev_t dev, uint16_t RegisterAdress, uint32_t *value)
+{
+	uint8_t status = 255;
+	
+	/* To be filled by customer. Return 0 if OK */
+	/* Warning : For big endian platforms, fields 'RegisterAdress' and 'value' need to be swapped. */
+	
+	return status;
+}
+
+uint8_t VL53L4CD_RdWord(Dev_t dev, uint16_t RegisterAdress, uint16_t *value)
+{
+	uint8_t status = 255;
+	
+	/* To be filled by customer. Return 0 if OK */
+	/* Warning : For big endian platforms, fields 'RegisterAdress' and 'value' need to be swapped. */
+	
+	return status;
+}
+
+uint8_t VL53L4CD_RdByte(Dev_t dev, uint16_t RegisterAdress, uint8_t *value)
+{
+	uint8_t status = 255;
+	
+	/* To be filled by customer. Return 0 if OK */
+	/* Warning : For big endian platforms, fields 'RegisterAdress' and 'value' need to be swapped. */
+	
+	return status;
+}
+
+uint8_t VL53L4CD_WrByte(Dev_t dev, uint16_t RegisterAdress, uint8_t value)
+{
+	uint8_t status = 255;
+
+	/* To be filled by customer. Return 0 if OK */
+	/* Warning : For big endian platforms, fields 'RegisterAdress' and 'value' need to be swapped. */
+	
+	return status;
+}
+
+uint8_t VL53L4CD_WrWord(Dev_t dev, uint16_t RegisterAdress, uint16_t value)
+{
+	uint8_t status = 255;
+	
+	/* To be filled by customer. Return 0 if OK */
+	/* Warning : For big endian platforms, fields 'RegisterAdress' and 'value' need to be swapped. */
+	
+	return status;
+}
+
+uint8_t VL53L4CD_WrDWord(Dev_t dev, uint16_t RegisterAdress, uint32_t value)
+{
+	uint8_t status = 255;
+
+	/* To be filled by customer. Return 0 if OK */
+	/* Warning : For big endian platforms, fields 'RegisterAdress' and 'value' need to be swapped. */
+	
+	return status;
+}
+
+uint8_t VL53L4CD_WaitMs(Dev_t dev, uint32_t TimeMs)
+{
+	uint8_t status = 255;
+	/* To be filled by customer */
+	return status;
+}
