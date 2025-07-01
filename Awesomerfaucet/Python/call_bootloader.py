6cdaf925ae6a9092e8e781965c3ed439c299e562
Steve M <Xenomorphxx131@gmail.com>
2025-07-01 16:44:13 -0400

    Fix for Python 3

--- Awesomerfaucet/Python/call_bootloader.py
+++ Awesomerfaucet/Python/call_bootloader.py
@@ -1,7 +1,7 @@
 import serial, time
-port = raw_input("Comport: COM")
+port = input("Comport: COM")
 foo = serial.Serial("COM{}".format(port))
-foo.write("System:rst:btlo;")
+foo.write(b"System:rst:btlo;")
 foo.close()
 time.sleep(7)
 # raw_input("Hit enter once Atmel device appears in device manager...")
