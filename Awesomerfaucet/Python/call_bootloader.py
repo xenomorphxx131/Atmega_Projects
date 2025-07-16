924f0d91e22baf28349571acf544502c810195aa
Steve M <Xenomorphxx131@gmail.com>
2025-07-16 18:53:11 -0400

    Consolidating the project back to just the Awesomefaucet folder. It now includes a hook in the make file and in the sensor dependent files to select which sensor to talk to. All possibly common code is now truly common.
    MAY GOD HAVE MERCY ON MY SOUL!

--- Awesomerfaucet/Python/call_bootloader.py
+++
@@ -1,8 +0,0 @@
-import serial, time
-port = input("Comport: COM")
-foo = serial.Serial("COM{}".format(port))
-foo.write(b"System:rst:btlo;")
-foo.close()
-time.sleep(7)
-# raw_input("Hit enter once Atmel device appears in device manager...")
-
