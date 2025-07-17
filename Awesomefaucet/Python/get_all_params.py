323b48bea813105c7fdfd85c7733e52f44db566e
Steve M <Xenomorphxx131@gmail.com>
2025-07-16 20:54:35 -0400

    Accepts Y or y now on program prompt. Commands changed to "SET" from "STORe"

--- Awesomefaucet/Python/get_all_params.py
+++ Awesomefaucet/Python/get_all_params.py
@@ -4,18 +4,15 @@ from datetime import datetime
 int_factory = lab_interfaces.interface_factory()
 sensor = int_factory.get_visa_serial_interface("COM" + input("Enter COM Port Number: COM"), timeout=1)
 
-if input("Program Device?") == "Y":
-    sensor.write('STORe:IIR_ALPHA -1.7')
-    sensor.write('STORe:IIR_BETA 0.72')
-    # sensor.write('STORe:IIR_ALPHA -1.8')
-    # sensor.write('STORe:IIR_BETA 0.81')
-    sensor.write('STORe:LASERPOWER 3')
-    sensor.write('STORe:DETECTION_THRESHOLD_MM 2')
-    sensor.write('STORe:MAX_DISTANCE_LEAKAGE 0.001666')
-    sensor.write(f'STORe:WATER_DEBOUNCE_TIMEOUT {round(2/0.001024)}')
-
-
-
+if input("Program Device?") in ["Y", "y"]:
+    sensor.write('SET:IIR_ALPHA -1.7')
+    sensor.write('SET:IIR_BETA 0.72')
+    # sensor.write('SET:IIR_ALPHA -1.8')
+    # sensor.write('SET:IIR_BETA 0.81')
+    sensor.write('SET:LASERPOWER 1')
+    sensor.write('SET:DETECTION_THRESHOLD_MM 2')
+    sensor.write('SET:MAX_DISTANCE_LEAKAGE 0.001666')
+    sensor.write(f'SET:WATER_DEBOUNCE_TIMEOUT {round(2/0.001024)}')
 
 print(f"RANGE: {sensor.ask('GET:RANGE?')}mm")
 print(f"IIR_ALPHA: {sensor.ask('GET:IIR_ALPHA?')}")
