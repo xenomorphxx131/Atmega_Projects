c61bf661c83adc25468b15a5517143829d31a31d
Steve M <Xenomorphxx131@gmail.com>
2025-08-13 12:41:54 -0400

    Cosmetic, spaces.

--- Awesomefaucet/src/Awesomefaucet.h
+++ Awesomefaucet/src/Awesomefaucet.h
@@ -7,10 +7,10 @@
 #ifndef AWESOMERFAUCET_H_
 #define AWESOMERFAUCET_H_
 
-	#include <util/delay.h> 				// Needed for _delay_ms()
-	#include <avr/interrupt.h>              // Needed for cli() and sei()
-	#include <VL6180X_SM.h>                 // Needed for sensor readings
-	#include <scpi_parser.h>                // scpi_process_cmd_P
+    #include <util/delay.h>                 // Needed for _delay_ms()
+    #include <avr/interrupt.h>              // Needed for cli() and sei()
+    #include <VL6180X_SM.h>                 // Needed for sensor readings
+    #include <scpi_parser.h>                // scpi_process_cmd_P
     #include <vl53l4cd_api.h>
     #include "Awesomefaucet_scpi.h"
     #include "Awesomefaucet_laser.h"
