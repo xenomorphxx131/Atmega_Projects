fc6d50517dda83812614e1c91377ad5900f6ce46
Steve M <Xenomorphxx131@gmail.com>
2025-08-13 12:52:41 -0400

    Changed to long form commands. Used during parser rewrite testing.

--- Awesomefaucet/Python/call_bootloader.py
+++ Awesomefaucet/Python/call_bootloader.py
@@ -1,6 +1,6 @@
 import serial, time
 port = input("Comport: COM")
 foo = serial.Serial("COM{}".format(port))
-foo.write(b"System:rst:btlo;")
+foo.write(b"SYSTem:RST:BTLOader;")
 foo.close()
 time.sleep(7)
