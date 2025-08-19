820b5169b22fb9cba967b10b8be7704940f0307b
Steve M <Xenomorphxx131@gmail.com>
2025-08-19 11:27:32 -0400

    cosmetic

--- Awesomefaucet/src/Awesomefaucet_timers.h
+++ Awesomefaucet/src/Awesomefaucet_timers.h
@@ -7,10 +7,10 @@
 #ifndef AWESOMERFAUCET_TIMERS_H_
 #define AWESOMERFAUCET_TIMERS_H_
 
-	#include <stdbool.h>                // Needed for booleans
-	#include <stdint.h>                 // Needed for integers
-	#include "Awesomefaucet_water.h"    // Needed to control water
+    #include <stdbool.h>                // Needed for booleans
+    #include <stdint.h>                 // Needed for integers
+    #include "Awesomefaucet_water.h"    // Needed to control water
 
-	void process_soft_timers(void);
+    void process_soft_timers(void);
 
 #endif
